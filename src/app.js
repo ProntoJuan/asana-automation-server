9b716185be2e043e98b2bc530b33ceb5d721f4ec
Juan F. Gomez <gomezjuanefe@gmail.com>
2024-12-11 11:20:04 -0500

    config.js modified

--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 import express from 'express'
-
-import { asanaConfig } from './config/asana.js'
 import configExpress from './config/express.js'
-import routes from './routes.js'
-import { configJsonDB } from './config/jsonDB.js'
 
 const app = express()
 const port = process.env.PORT || 8080
 
 configExpress(app)
-routes(app)
-asanaConfig()
-configJsonDB()
 
 app.listen(port, () => {
   console.log('Server started on port 8080')
