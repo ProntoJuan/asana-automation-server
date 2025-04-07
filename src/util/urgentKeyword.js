d6656d8d5751414caadbd7def04d2d03b7adea4e
Juan F. Gomez <gomezjuanefe@gmail.com>
2025-04-07 18:12:09 -0500

    Add exclusion for 'not urgent' in urgent keyword check

--- src/util/urgentKeyword.js
+++ src/util/urgentKeyword.js
@@ -1,4 +1,13 @@
 export const containsUrgentKeyword = (urgentKeywords, notes) => {
+  const excludedPatterns = [
+    /\bnot urgent\b/i
+  ]
+  const isExcluded = excludedPatterns.some((pattern) => pattern.test(notes))
+
+  if (isExcluded) {
+    return false
+  }
+
   return urgentKeywords.some((keyword) => {
     const regex = new RegExp(`\\b${keyword}\\b`, 'i')
     return regex.test(notes)
